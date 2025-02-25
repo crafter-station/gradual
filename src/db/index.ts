75af412e9cae3d92a8ce91b1745842c7cee65887
Anthony Cueva <hi@cueva.io>
2025-02-25 03:42:26 -0500

    fix(chore): from pooled to unpooled postgres

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,15 +1,15 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 
 import * as schema from './schema';
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set');
+if (!process.env.DATABASE_URL_UNPOOLED) {
+  throw new Error('DATABASE_URL_UNPOOLED is not set');
 }
 declare global {
   var db: ReturnType<typeof drizzle<typeof schema>> | undefined;
 }
 
 if (!global.db) {
-  global.db = drizzle(process.env.DATABASE_URL, { schema });
+  global.db = drizzle(process.env.DATABASE_URL_UNPOOLED, { schema });
 }
 
 // biome-ignore lint/suspicious/noRedeclare: <explanation>
