b03e0be4e37277c0efd05af415e3061e68132835
Anthony Cueva <hi@cueva.io>
2025-02-28 14:14:03 -0500

    feat: auth middleware + revalidation

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
-// middleware.ts
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { createI18nMiddleware } from 'next-international/middleware';
-import type { NextRequest } from 'next/server';
 
 const I18nMiddleware = createI18nMiddleware({
   locales: ['en', 'es'],
@@ -8,10 +7,18 @@ const I18nMiddleware = createI18nMiddleware({
   urlMappingStrategy: 'rewrite',
 });
 
-export function middleware(request: NextRequest) {
+const isProtectedRoute = createRouteMatcher(['dashboard/(.*)']);
+
+export default clerkMiddleware(async (auth, request) => {
+  if (isProtectedRoute(request)) await auth.protect();
   return I18nMiddleware(request);
-}
+});
 
 export const config = {
-  matcher: ['/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
