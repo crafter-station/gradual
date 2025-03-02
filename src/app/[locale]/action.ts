7ecf45baee1e7eaee294ec296cb8dd0ce35b1157
Jibaru <ignacioruedaboada@hotmail.com>
2025-03-02 14:08:16 -0500

    refactor: call join wait list usecase

--- src/app/[locale]/action.ts
+++ src/app/[locale]/action.ts
@@ -1,18 +1,12 @@
 'use server';
-import { db } from '@/db';
-import { waitlist } from '@/db/schema/waitlist';
-import { welcomeEmail } from '@/emails/helpers/waitlist-emails';
-import { EMAIL_SENDER } from '@/lib/constants';
+import { useCase } from '@/core/usecases/container';
+import { JoinWaitlistUseCase } from '@/core/usecases/join-waitlist.usecase';
 import type { ActionState } from '@/lib/utils';
-import { eq } from 'drizzle-orm';
-import { Resend } from 'resend';
 
 export type JoinWaitlistState = ActionState<{
   name: string;
   email: string;
 }>;
-
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function joinWaitlist(
   prevState: JoinWaitlistState | undefined,
@@ -21,29 +15,7 @@ export async function joinWaitlist(
   try {
     const name = formData.get('name') as string;
     const email = formData.get('email') as string;
-
-    if (!email) return { success: false, error: 'Email is required' };
-
-    const existing = await db
-      .select()
-      .from(waitlist)
-      .where(eq(waitlist.email, email));
-
-    if (existing.length > 0)
-      return { success: false, error: 'Email already registered' };
-
-    await db.insert(waitlist).values({ name, email }).execute();
-
-    const emailTemplate = welcomeEmail(name);
-
-    await resend.emails.send({
-      from: EMAIL_SENDER,
-      to: email,
-      subject: emailTemplate.subject,
-      html: emailTemplate.html,
-    });
-
-    return { success: true };
+    return await useCase(JoinWaitlistUseCase).execute(name, email);
   } catch (error) {
     return { success: false, error: 'Something went wrong' };
   }
