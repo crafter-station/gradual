feca2f6fb8e80e5ac5a206fd575e1318cfaa2d89
Jibaru <ignacioruedaboada@hotmail.com>
2025-03-08 16:52:42 -0500

    feat: implement prompt to generate alternative steps and rating

--- src/core/usecases/create-alternative-steps.usecase.ts
+++ src/core/usecases/create-alternative-steps.usecase.ts
@@ -9,6 +9,12 @@ import {
 import type { StepRepo } from '@/core/domain/step-repo';
 import { AlternativeStep } from '../domain/alternative-step';
 import { v4 as uuidv4 } from 'uuid';
+import { generateAlternativesWithRatingPromptForQuestionStep } from '@/lib/prompts/generate-alternative-steps';
+
+export interface CreateAlternativeStepsUseCaseResp {
+  rating: string;
+  alternativeSteps: AlternativeStep[];
+}
 
 export class CreateAlternativeStepsUseCase {
   constructor(
@@ -21,73 +27,69 @@ export class CreateAlternativeStepsUseCase {
     originalStepId: string,
     answeredSeconds: number,
     answer: string,
-  ): Promise<{ rating: string; alternativeSteps: AlternativeStep[] }> {
+  ): Promise<CreateAlternativeStepsUseCaseResp> {
     const step = await this.stepRepo.findById(originalStepId);
     if (step === null) {
       throw new Error('Step not found');
     }
 
-    if (step.type === QUESTION_STEP_TYPE) {
-      const content = step.content as QuestionStepContent;
-      const prompt = this.promptToGenerateNewAlternatives(
-        content.correctAlternative === answer,
-        answeredSeconds,
-        step,
-      );
-
-      // TODO: refactor schema to a constant
-      const aiResponse = await this.aiGenerator.generateObject({
-        model: 'o3-mini',
-        prompt: prompt,
-        schema: z.object({
-          rating: z.string(),
-          contents: z.array(
-            z.object({
-              type: z.literal(QUESTION_STEP_TYPE),
-              questionBody: z.string(),
-              correctAlternative: z.string(),
-              correctAlternativeExplanation: z.string(),
-              distractors: z.array(
-                z.object({
-                  alternative: z.string(),
-                  explanation: z.string(),
-                }),
-              ),
-            }),
-          ),
-        }),
-        experimental_telemetry: {
-          isEnabled: true,
-          functionId: 'generate-alternative-steps',
-        },
-      });
-
-      const alternativeSteps: AlternativeStep[] = [];
-      for (const content of aiResponse.contents) {
-        alternativeSteps.push(
-          new AlternativeStep(uuidv4(), content, originalStepId),
-        );
-      }
-
-      await this.alternativeStepRepo.storeMany(alternativeSteps);
-
-      // TODO: store rating into the answered question by the user
-
-      return {
-        rating: aiResponse.rating,
-        alternativeSteps,
-      };
+    switch (step.type) {
+      case QUESTION_STEP_TYPE:
+        return await this.handleQuestionStep(step, answeredSeconds, answer);
     }
 
     throw new Error('Not supported yet');
   }
 
-  private promptToGenerateNewAlternatives(
-    hasAnsweredOk: boolean,
-    answeredSeconds: number,
+  async handleQuestionStep(
     step: Step,
-  ): string {
-    // TODO: add prompt
-    return '';
+    answeredSeconds: number,
+    answer: string,
+  ): Promise<CreateAlternativeStepsUseCaseResp> {
+    const content = step.content as QuestionStepContent;
+
+    const aiResponse = await this.aiGenerator.generateObject({
+      model: 'o3-mini',
+      prompt: generateAlternativesWithRatingPromptForQuestionStep(
+        content.correctAlternative === answer,
+        answeredSeconds,
+        step,
+      ),
+      schema: z.object({
+        rating: z.string(),
+        contents: z.array(
+          z.object({
+            type: z.literal(QUESTION_STEP_TYPE),
+            questionBody: z.string(),
+            correctAlternative: z.string(),
+            correctAlternativeExplanation: z.string(),
+            distractors: z.array(
+              z.object({
+                alternative: z.string(),
+                explanation: z.string(),
+              }),
+            ),
+          }),
+        ),
+      }),
+      experimental_telemetry: {
+        isEnabled: true,
+        functionId: 'generate-alternative-steps',
+      },
+    });
+
+    const alternativeSteps: AlternativeStep[] = [];
+    for (const content of aiResponse.contents) {
+      alternativeSteps.push(
+        new AlternativeStep(uuidv4(), QUESTION_STEP_TYPE, content, step.id),
+      );
+    }
+
+    await this.alternativeStepRepo.storeMany(alternativeSteps);
+
+    return {
+      rating: aiResponse.rating,
+      alternativeSteps,
+    };
   }
 }
