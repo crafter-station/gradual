85c9202ae166ffd5fcab2619065ac9b36ac0ac03
Jibaru <ignacioruedaboada@hotmail.com>
2025-03-09 16:45:10 -0500

    feat: improve schema

--- src/db/schema/alternative-step/index.ts
+++ src/db/schema/alternative-step/index.ts
@@ -5,6 +5,8 @@ import { FillInTheBlankStepContentSchema } from '../step/fill-in-the-blank';
 import { MultipleChoiceQuestionStepContentSchema } from '../step/multiple-choice';
 import { QuestionStepContentSchema } from '../step/question';
 import { step } from '../step';
+import { relations } from 'drizzle-orm';
+import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 
 export const AlterantiveStepContentSchema = z.union([
   // Evaluated steps
@@ -20,7 +22,7 @@ export const AlternativeStepTypes = AlterantiveStepContentSchema.options.map(
 
 export type AlternativeStepType = (typeof AlternativeStepTypes)[number];
 
-export const alternativeStepTypeEnum = pgEnum('STEP_TYPE_ENUM', [
+export const alternativeStepTypeEnum = pgEnum('ALTERNATIVE_STEP_TYPE_ENUM', [
   AlternativeStepTypes[0],
   ...AlternativeStepTypes.slice(1),
 ]);
@@ -35,7 +37,7 @@ export type AlternativeStepContent = z.infer<
 >;
 
 export const alternativeStep = pgTable(
-  'step',
+  'alternative_step',
   {
     id: uuid('id').primaryKey().defaultRandom(),
 
@@ -55,4 +57,20 @@ export const alternativeStep = pgTable(
       .defaultNow(),
   },
   (table) => [],
+);
+
+export const InsertAlternativeStepSchema = createInsertSchema(alternativeStep);
+export const SelectAlternativeStepSchema = createSelectSchema(alternativeStep);
+
+export type SelectAlternativeStep = typeof alternativeStep.$inferSelect;
+export type InsertAlternativeStep = typeof alternativeStep.$inferInsert;
+
+export const alternativeStepRelations = relations(
+  alternativeStep,
+  ({ one }) => ({
+    step: one(step, {
+      fields: [alternativeStep.stepId],
+      references: [step.id],
+    }),
+  }),
 );
